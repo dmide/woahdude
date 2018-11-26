70c7d44112e721389d0ded525127bab798570a76
Dmide <revdmide@gmail.com>
2018-11-26 16:43:37 +0300

    glide timeouts increased

--- app/src/main/kotlin/com/reddit/woahdude/common/GlideModule.kt
+++ app/src/main/kotlin/com/reddit/woahdude/common/GlideModule.kt
@@ -1,7 +1,27 @@
 package com.reddit.woahdude.common
 
+import android.content.Context
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.load.model.GlideUrl
+import okhttp3.OkHttpClient
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import java.io.InputStream
+import java.util.concurrent.TimeUnit
+
 
 @GlideModule
-class MyAppGlideModule : AppGlideModule()
+class MyAppGlideModule : AppGlideModule() {
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val builder = OkHttpClient.Builder()
+        builder.readTimeout(20, TimeUnit.SECONDS)
+        builder.writeTimeout(20, TimeUnit.SECONDS)
+        builder.connectTimeout(20, TimeUnit.SECONDS)
+        builder.retryOnConnectionFailure(true)
+        registry.append(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory(builder.build()))
+    }
+
+}
